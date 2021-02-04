d835a2c1e9ea5598f6d03b07c5644867c2715b06
Mira <matsakovamira156@gmail.com>
2021-02-04 13:41:54 +0200

    fix bug with mailer

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,19 +13,6 @@ set :linked_dirs,  fetch(:linked_dirs, []).push("log", "tmp/pids", "tmp/cache", 
 set :config_example_suffix, ".example"
 set :config_files, ["config/database.yml", "config/secrets.yml"]
 set :nginx_use_ssl, false
-set :puma_rackup, -> { File.join(current_path, 'config.ru') }
-set :puma_state, "#{shared_path}/tmp/pids/puma.state"
-set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
-set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock" # accepts array for multi-bind
-set :puma_conf, "#{shared_path}/puma.rb"
-set :puma_access_log, "#{shared_path}/log/puma_access.log"
-set :puma_error_log, "#{shared_path}/log/puma_error.log"
-set :puma_role, :app
-set :puma_threads, [0, 8]
-set :puma_workers, 0
-set :puma_worker_timeout, nil
-set :puma_init_active_record, true
-set :puma_preload_app, false
 
 namespace :deploy do
   before 'check:linked_files', 'set:master_key'
--- config/initializers/app_env_vars.rb
+++
@@ -1,2 +0,0 @@
-ENV['MAILER_EMAIL'] = 'matsakovamira156@gmail.com'
-ENV['MAILER_PASSWORD'] = 'nozomo89123'
