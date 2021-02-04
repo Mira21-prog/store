a7033b6be0ba032d0978d6c678730c7dadce7af0
Mira <matsakovamira156@gmail.com>
2021-02-04 10:42:20 +0200

    change deploy file

--- config/deploy.rb
+++ config/deploy.rb
@@ -42,12 +42,13 @@ namespace :deploy do
     end
   end
 end
-desc "tail production log files"
-task :tail_logs, :roles => :app do
-  trap("INT") { puts 'Interupted'; exit 0; }
-  run "tail -f #{shared_path}/log/production.log" do |channel, stream, data|
-    puts  # for an extra line break before the host name
-    puts "#{channel[:host]}: #{data}"
-    break if stream == :err
-  end
-end
+
+# desc "tail production log files"
+# task :tail_logs, :roles => :app do
+#   trap("INT") { puts 'Interupted'; exit 0; }
+#   run "tail -f #{shared_path}/log/production.log" do |channel, stream, data|
+#     puts  # for an extra line break before the host name
+#     puts "#{channel[:host]}: #{data}"
+#     break if stream == :err
+#   end
+# end
