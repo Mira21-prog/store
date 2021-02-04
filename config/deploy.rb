b0837ea31a1faf1554717883f0ccb54089975bbd
Mira <matsakovamira156@gmail.com>
2021-02-04 05:27:39 +0200

    fix bug with db

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,6 +13,7 @@ set :linked_dirs,  fetch(:linked_dirs, []).push("log", "tmp/pids", "tmp/cache", 
 set :config_example_suffix, ".example"
 set :config_files, ["config/database.yml", "config/secrets.yml"]
 set :nginx_use_ssl, false
+set :linked_dirs,  %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle .bundle public/system public/uploads storage}
 
 namespace :deploy do
   before 'check:linked_files', 'set:master_key'
